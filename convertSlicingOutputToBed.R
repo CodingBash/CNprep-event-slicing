f8d0ae12892defc2125309c192ef17e67979a3c2
CodingBash <bbecerr@outlook.com>
2018-07-27 21:06:48 -0400

    Output slicing files to BED files

---
+++ convertSlicingOutputToBed.R
@@ -0,0 +1,20 @@
+setwd("~/Git-Projects/Git-Research-Projects/CNprep-event-slicing/")
+source("helperFunctions.R")
+source("coreGenerationLibrary.R")
+
+table <- read.table("output/organoidSlices.txt", sep = "\t", header = TRUE)
+chromosomeSizes <- readRDS("resources/meta/chromosomeSizes.rds")
+
+ids <- unique(table$profID)
+
+for(id in ids){
+  probeBed <- table[table$profID == id,c(6,2,3,11)]
+  probeBed[[1]] <- paste0("chr", probeBed[[1]])
+  
+  genomeBed <- table[table$profID == id,c(6,9,10,11)]
+  genomeBed <- absoluteToChromosomeBPConversion(genomeBed, chromosomeSizes)
+  genomeBed[[1]] <- paste0("chr", genomeBed[[1]])
+  
+  write.table(probeBed, file = paste0("output/bed/", id, "_slicingProbes.bed"), sep="\t", quote=FALSE, row.names = FALSE, col.names = FALSE)
+  write.table(genomeBed, file = paste0("output/bed/", id, "_genomeBed.bed"), sep="\t", quote=FALSE, row.names = FALSE, col.names = FALSE)
+}
